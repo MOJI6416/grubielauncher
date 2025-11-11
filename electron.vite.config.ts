369a6fe9217cc5ee0a4e516f9fb669a645bf7578
Dmytro <profisional.akaunt@gmail.com>
2025-11-12 00:45:20 +0200

    fix

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, loadEnv } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 const alias = {
@@ -7,29 +7,44 @@ const alias = {
   '@renderer': resolve(__dirname, 'src/renderer/src')
 }
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()],
-    resolve: {
-      alias
-    }
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()],
-    resolve: {
-      alias
-    }
-  },
-  renderer: {
-    resolve: {
-      alias
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    main: {
+      plugins: [externalizeDepsPlugin()],
+      resolve: {
+        alias
+      }
     },
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'src/renderer/index.html'),
-          updater: resolve(__dirname, 'src/renderer/updater.html')
+    preload: {
+      plugins: [externalizeDepsPlugin()],
+      resolve: {
+        alias
+      },
+      define: {
+        'process.env.DISCORD_CLIENT_ID': JSON.stringify(env.MAIN_VITE_DISCORD_CLIENT_ID),
+        'process.env.DISCORD_CLIENT_PASSWORD': JSON.stringify(
+          env.MAIN_VITE_DISCORD_CLIENT_PASSWORD
+        ),
+        'process.env.MICROSOFT_CLIENT_ID': JSON.stringify(env.MAIN_VITE_MICROSOFT_CLIENT_ID),
+        'process.env.ELYBY_CLIENT_ID': JSON.stringify(env.MAIN_VITE_ELYBY_CLIENT_ID),
+        'process.env.ELYBY_CLIENT_SECRET': JSON.stringify(env.MAIN_VITE_ELYBY_CLIENT_SECRET),
+        'process.env.BACKEND_URL': JSON.stringify(env.MAIN_VITE_BACKEND_URL),
+        'process.env.CURSEFORGE_API_KEY': JSON.stringify(env.MAIN_VITE_CURSEFORGE_API_KEY)
+      }
+    },
+    renderer: {
+      resolve: {
+        alias
+      },
+      plugins: [react()],
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'src/renderer/index.html'),
+            updater: resolve(__dirname, 'src/renderer/updater.html')
+          }
         }
       }
     }
